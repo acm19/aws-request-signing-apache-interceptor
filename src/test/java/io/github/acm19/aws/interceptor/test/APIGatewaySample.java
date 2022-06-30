46cf385200119a1ee85933a939eaa2f363061b35
Daniel (dB.) Doubrovkine <dblock@amazon.com>
2022-06-30 17:08:26 -0300

    Updated README with a working OpenSearch sample.
    Signed-off-by: Daniel (dB.) Doubrovkine <dblock@amazon.com>

--- src/test/java/io/github/acm19/aws/interceptor/test/APIGatewaySample.java
+++ src/test/java/io/github/acm19/aws/interceptor/test/APIGatewaySample.java
@@ -1,6 +1,9 @@
 package io.github.acm19.aws.interceptor.test;
 
 import org.apache.http.client.methods.HttpGet;
+
+import software.amazon.awssdk.regions.Region;
+
 import java.io.IOException;
 
 public class APIGatewaySample extends Sample {
@@ -8,6 +11,7 @@ public class APIGatewaySample extends Sample {
      * The invoke URL for your API which is usually https://api_id.execute-api.api-region.amazonaws.com/stage
      */
     private static final String INVOKE_URL = "https://api_id.execute-api.api-region.amazonaws.com/stage";
+    private static final Region REGION = Region.US_EAST_1;
 
     public static void main(String[] args) throws IOException {
         APIGatewaySample apiGatewaySample = new APIGatewaySample();
@@ -16,6 +20,6 @@ public class APIGatewaySample extends Sample {
 
     private void makeAPIGGetRequest() throws IOException {
         HttpGet Http = new HttpGet(INVOKE_URL + "/some/path?and=param");
-        logRequest("execute-api", Http);
+        logRequest("execute-api", REGION, Http);
     }
 }
