45b115b5c742db34ce00a3aed35a4070c6c0d196
Daniel (dB.) Doubrovkine <dblock@amazon.com>
2022-07-06 12:38:32 -0300

    Added SPDX license headers.
    Signed-off-by: Daniel (dB.) Doubrovkine <dblock@amazon.com>

--- src/main/java/io/github/acm19/aws/interceptor/http/AwsRequestSigningApacheInterceptor.java
+++ src/main/java/io/github/acm19/aws/interceptor/http/AwsRequestSigningApacheInterceptor.java
@@ -1,3 +1,11 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The AWS Request Signing Interceptor Contributors require
+ * contributions made to this file be licensed under the
+ * Apache-2.0 license or a compatible open source license.
+ */
+
 package io.github.acm19.aws.interceptor.http;
 
 import static org.apache.http.protocol.HttpCoreContext.HTTP_TARGET_HOST;
